829b3e81351269b770c1936041f9ac27516ecd39
Phillipp Ohl <phillippohl@gmail.com>
2013-06-07 20:49:50 +0200

    Added interface 'Set'.
    Signed-off-by: Phillipp Ohl <phillippohl@gmail.com>

---
+++ src/interfaces/SetSpec.java
@@ -0,0 +1,16 @@
+package interfaces;
+/**
+ * 
+ */
+
+/**
+ * @author Phillipp Ohl
+ * @version 0.1
+ */
+public interface SetSpec {
+	void calculateSum();
+	void setLength();
+	int getLength();
+	int getRandomMember();
+	int getSum();
+}
